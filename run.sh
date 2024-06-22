be665031fdabc58743ec3993d885ca50fc078610
mike <mike@mathrules.us>
2024-06-22 04:25:35 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 make clean
 working_dir='/mnt/x/latex-example-expressions'
-find $working_dir/latex-example-expressions/examples_of_valid_latex/|grep clean|shuf|sed -n 1p|xargs -i cp "{}" test.tex
+find $working_dir/examples_of_valid_latex/|grep clean|shuf|sed -n 1p|xargs -i cp "{}" test.tex
 ./scanner.out .
 python read_offsets.py
 cat test.tex
